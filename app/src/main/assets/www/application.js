146cab26a1b130291efc8cbc1b07081609a8e211
Evanzyker <quentin.rimoux@live.fr>
2016-12-06 16:48:28 -0500

    Ajout zone de dessin fonctionelle

--- app/src/main/assets/www/application.js
+++ app/src/main/assets/www/application.js
@@ -1,81 +1,49 @@
 var applicationPaintGap = {
-  lancer:function()
-  {
-    this.PaintGapDAO = new PaintGapDAO();
+    lancer: function() {
+        this.PaintGapDAO = new PaintGapDAO();
 
-    
-    $(window).on('hashchange', $.proxy(this.naviguer, this));
-    
-    this.naviguer();
-  },
-  naviguer:function()
-  {
-    //alert("Naviguer selon : "+window.location.hash);
-    var ancre = window.location.hash;
-    if(!ancre)
-    {
-      this.PaintGapDAO.listerLesPaintGap($.proxy(this.afficherTousLesPaintGap, this)); 
-    }
-    else if(ancre.match(/^#ajouter-paintgap/)) 
-    {
-      this.ajouterPaintGapVue = new ajouterPaintGapVue();
-      this.ajouterPaintGapVue.afficher($.proxy(this.sauvegarderNouveauPaintGap, this)); 
-    }
-    else if(ancre.match(/^#modifier-paintgap\/([0-9]+)/)) 
-    {
-      var trouvailles = ancre.match(/^#modifier-paintgap\/([0-9]+)/);
-      var id_paintgap = trouvailles[1];
-      var paintgap = this.PaintGapDAO.trouverPaintGapParId(id_paintgap);
-      this.ModifierPaintGapVue = new ModifierPaintGapVue(paintgap);
-      this.ModifierPaintGapVue.afficher($.proxy(this.modifierPaintGap, this)); 
-    }
-    else
-    {
-      var trouvailles = ancre.match(/^#paintgap\/([0-9]+)/);
-      var id_paintgap = trouvailles[1];
-      var paintgap = this.PaintGapDAO.trouverPaintGapParId(id_paintgap);
-      this.PaintGapVue = new PaintGapVue(paintgap);
-      this.PaintGapVue.afficher();    
-    }
-  },
-  
-  sauvegarderNouveauPaintGap:function(paintgap)
-  {
-    this.PaintGapDAO.ajouterPaintGap(paintgap);
-  },
 
-  modifierPaintGap:function(paintgap)
-  {
-    this.PaintGapDAO.modifierPaintGap(paintgap);
-  },
+        $(window).on('hashchange', $.proxy(this.naviguer, this));
 
-  
-  afficherTousLesPaintGap:function(listePaintGap)
-  {
-    this.ListePaintGapVue = new ListePaintGapVue(listePaintGap);
-    this.ListePaintGapVue.afficher();      
-  }
-  
+        this.naviguer();
+    },
+    naviguer: function() {
+        //alert("Naviguer selon : "+window.location.hash);
+        var ancre = window.location.hash;
+        if (!ancre) {
+            this.PaintGapDAO.listerLesPaintGap($.proxy(this.afficherTousLesPaintGap, this));
+        } else if (ancre.match(/^#ajouter-paintgap/)) {
+            this.ajouterPaintGapVue = new ajouterPaintGapVue();
+            this.ajouterPaintGapVue.afficher($.proxy(this.sauvegarderNouveauPaintGap, this));
+        } else if (ancre.match(/^#modifier-paintgap\/([0-9]+)/)) {
+            var trouvailles = ancre.match(/^#modifier-paintgap\/([0-9]+)/);
+            var id_paintgap = trouvailles[1];
+            var paintgap = this.PaintGapDAO.trouverPaintGapParId(id_paintgap);
+            this.ModifierPaintGapVue = new ModifierPaintGapVue(paintgap);
+            this.ModifierPaintGapVue.afficher($.proxy(this.modifierPaintGap, this));
+        } else {
+            var trouvailles = ancre.match(/^#paintgap\/([0-9]+)/);
+            var id_paintgap = trouvailles[1];
+            var paintgap = this.PaintGapDAO.trouverPaintGapParId(id_paintgap);
+            this.PaintGapVue = new PaintGapVue(paintgap);
+            this.PaintGapVue.afficher();
+        }
+    },
+
+    sauvegarderNouveauPaintGap: function(paintgap) {
+        this.PaintGapDAO.ajouterPaintGap(paintgap);
+    },
+
+    modifierPaintGap: function(paintgap) {
+        this.PaintGapDAO.modifierPaintGap(paintgap);
+    },
+
+
+    afficherTousLesPaintGap: function(listePaintGap) {
+        this.ListePaintGapVue = new ListePaintGapVue(listePaintGap);
+        this.ListePaintGapVue.afficher();
+    }
+
 };
 
 applicationPaintGap.lancer();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/main/assets/www/vue/AjouterPaintGapVue.js
+++ app/src/main/assets/www/vue/AjouterPaintGapVue.js
@@ -1,27 +1,23 @@
-var ajouterPaintGapVue = function()
-{
-  
-  this.afficher = function(actionAjouterPaintGap)
-  {
-    $("body").html(ajouterPaintGapVue.html);
-    
-    $("#formulaire-ajouter").on("submit", $.proxy(this.ajouterPaintGap, this));
-    
-    this.actionAjouterPaintGap = actionAjouterPaintGap;
-    
-  }
-  
-  this.ajouterPaintGap = function()
-  {
-			var nom = $("#paintgap-nom").val();
-			var description = $("#paintgap-description").val();
-      var paintgap = new PaintGap(id=null,nom,description);
-      this.actionAjouterPaintGap(paintgap);
-      
-			window.location.hash = "";
-			event.preventDefault();		
-    
-  }
+var ajouterPaintGapVue = function() {
+
+    this.afficher = function(actionAjouterPaintGap) {
+        $("body").html(ajouterPaintGapVue.html);
+
+        $("#formulaire-ajouter").on("submit", $.proxy(this.ajouterPaintGap, this));
+
+        this.actionAjouterPaintGap = actionAjouterPaintGap;
+
+    }
+
+    this.ajouterPaintGap = function() {
+        var nom = $("#paintgap-nom").val();
+        var description = $("#paintgap-description").val();
+        var paintgap = new PaintGap(id = null, nom, description);
+        this.actionAjouterPaintGap(paintgap);
+
+        window.location.hash = "";
+        event.preventDefault();
+
+    }
 }
 ajouterPaintGapVue.html = $("#page-ajouter-paintgap").html();
-
